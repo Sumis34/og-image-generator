d0549713f79a4222feb99b0b55c40187bdb0a993
Noe <eon.sberk@gmail.com>
2022-05-22 12:11:42 +0200

    seperate comp

--- src/pages/api/rust.tsx
+++ src/pages/api/rust.tsx
@@ -5,7 +5,7 @@ interface QueryParams {
   name: string;
   steamid: string;
   avatar: string;
-  local: 'en' | 'de';
+  local: string;
 }
 
 const style = `
@@ -32,82 +32,86 @@ tailwind.config = {
 
 export default withOGImage<'query', QueryParams>({
   template: {
-    react: ({ name, steamid, avatar, local }) => {
-      return (
-        <html>
-          <head>
-            <style dangerouslySetInnerHTML={{ __html: style }} />
-            <Script src='https://cdn.tailwindcss.com'></Script>
-            <script dangerouslySetInnerHTML={{ __html: script }} />
-          </head>
-          <body className='bg-background h-screen w-screen'>
-            <main className='relative h-full w-full'>
-              <div
-                style={{
-                  backgroundImage: `url(${avatar})`,
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                  position: 'absolute',
-                  inset: '0px',
-                  width: '100%',
-                  height: '100%',
-                }}
-              />
-              <div
-                style={{
-                  background:
-                    'linear-gradient(0deg, rgba(8,8,8,1) 20%, rgba(16,16,16,0.80) 100%)',
-                  position: 'absolute',
-                  inset: '0px',
-                  width: '100%',
-                  height: '100%',
-                }}
-              />
-              <div className='relative flex justify-between flex-col h-full'>
-                <div className='flex gap-7'>
-                  <img src={avatar} alt='avatar' width={120} />
-                  <div>
-                    <h1
-                      className='text-8xl text-sand'
-                      style={{ fontFamily: 'Bebas Neue' }}
-                    >
-                      {name}
-                    </h1>
-                    <p className='text-sand opacity-70 text-4xl font-bold'>
-                      {steamid}
-                    </p>
-                  </div>
-                </div>
-                <div className='flex text-4xl items-center gap-2 stroke-rust'>
-                  <h2 className='text-rust font-bold text-4xl'>
-                    {local === 'de' ? 'Profil ansehen' : 'View profile'}
-                  </h2>
-                  <svg
-                    stroke='#cd412b'
-                    fill='#cd412b'
-                    strokeWidth='0'
-                    viewBox='0 0 20 20'
-                    height='1em'
-                    width='1em'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
-                      clipRule='evenodd'
-                    ></path>
-                  </svg>
-                </div>
-              </div>
-            </main>
-          </body>
-        </html>
-      );
-    },
+    react: ({ name, steamid, avatar, local }) => (
+      <Template name={name} steamid={steamid} avatar={avatar} local={local} />
+    ),
   },
   cacheControl: 'public, max-age=604800, immutable',
   dev: {
     inspectHtml: false,
   },
 });
+
+function Template({ name, steamid, avatar, local }: QueryParams) {
+  return (
+    <html>
+      <head>
+        <style dangerouslySetInnerHTML={{ __html: style }} />
+        <Script src='https://cdn.tailwindcss.com'></Script>
+        <script dangerouslySetInnerHTML={{ __html: script }} />
+      </head>
+      <body className='bg-background h-screen w-screen'>
+        <main className='relative h-full w-full'>
+          <div
+            style={{
+              backgroundImage: `url(${avatar})`,
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              position: 'absolute',
+              inset: '0px',
+              width: '100%',
+              height: '100%',
+            }}
+          />
+          <div
+            style={{
+              background:
+                'linear-gradient(0deg, rgba(8,8,8,1) 20%, rgba(16,16,16,0.80) 100%)',
+              position: 'absolute',
+              inset: '0px',
+              width: '100%',
+              height: '100%',
+            }}
+          />
+          <div className='relative flex justify-between flex-col h-full'>
+            <div className='flex gap-7'>
+              <img src={avatar} alt='avatar' width={120} />
+              <div>
+                <h1
+                  className='text-8xl text-sand'
+                  style={{ fontFamily: 'Bebas Neue' }}
+                >
+                  {name}
+                </h1>
+                <p className='text-sand opacity-70 text-4xl font-bold'>
+                  {steamid}
+                </p>
+              </div>
+            </div>
+            <div className='flex text-4xl items-center gap-2 stroke-rust'>
+              <h2 className='text-rust font-bold text-4xl'>
+                {local === 'de' ? 'Profil ansehen' : 'View profile'}
+              </h2>
+              <svg
+                stroke='#cd412b'
+                fill='#cd412b'
+                strokeWidth='0'
+                viewBox='0 0 20 20'
+                height='1em'
+                width='1em'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
+                  clipRule='evenodd'
+                ></path>
+              </svg>
+            </div>
+          </div>
+        </main>
+      </body>
+    </html>
+  );
+}
